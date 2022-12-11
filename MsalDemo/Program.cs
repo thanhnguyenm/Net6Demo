c4696e7eea927f3ce6ace50580eaa10d9988f1c5
Thanh Nguyen <thanh.nguyen.minh@positivethinking.tech>
2022-12-11 23:48:01 +0700

    DevOps training

---
+++ MsalDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Identity.Client;
+
+var _clientId = "";//"de92701e-6b30-427e-b2bd-5d895f02ac72";
+var _tenantId = "";// "9091782d-c600-47a5-9d3c-4c11ddeb7628";
+
+
+
+var app = PublicClientApplicationBuilder
+    .Create(_clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+string[] scopes = { "user.read" };
+
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+Console.WriteLine($"Token {result.AccessToken}");
